62697ff52fefe0c1cbb55b7fc84135f0c019554c
TeodorStevanovic <stevanovicteodor002@gmail.com>
2025-10-31 13:23:16 +0100

    Implemented Contact component structure and base styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,30 @@
 import React, { useContext } from "react";
 import LanguageContext from "../../context/LanguageContext";
+import theme_pattern from "../../assets/theme_pattern.svg";
 import "./Contact.css";
 
 const Contact = () => {
-    const {language} = useContext(LanguageContext);
-    const texts = {
-        sr: {title: "Kontaktiraj me"},
-        en: {title: "Contact me"}
-    }
+  const { language } = useContext(LanguageContext);
+  const texts = {
+    sr: { title: "Kontaktiraj me" },
+    en: { title: "Contact me" },
+  };
   return (
     <section id="contact" className="contact-container">
-      <h1 className="contact-title">{texts[language].title}</h1>
+      <div className="contact-title">
+        <h1>{texts[language].title}</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>
+            Feel free to reach out if you have any questions, suggestions, or
+            collaboration ideas. I’ll get back to you as soon as possible.
+          </p>
+        </div>
+        <div className="contact-right"></div>
+      </div>
     </section>
   );
 };
