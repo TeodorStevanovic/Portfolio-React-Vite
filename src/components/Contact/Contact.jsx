87463be94a5929a9c4ba85595fb51d522e12a24f
TeodorStevanovic <stevanovicteodor002@gmail.com>
2025-11-10 13:29:29 +0100

    Refined Contact component with localized Serbian translation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,18 @@ import "./Contact.css";
 const Contact = () => {
   const { language } = useContext(LanguageContext);
   const texts = {
-    sr: { title: "Kontaktiraj me" },
-    en: { title: "Contact me" },
+    sr: {
+      title: "Kontaktiraj me",
+      titleLeft: "Započnimo razgovor",
+      description:
+        "Slobodno me kontaktiraj ako imaš bilo kakva pitanja, sugestije ili ideje za saradnju. Odgovoriću ti čim bude moguće.",
+    },
+    en: {
+      title: "Contact me",
+      titleLeft: "Let's Talk",
+      description:
+        "Feel free to reach out if you have any questions, suggestions, or collaboration ideas. I’ll get back to you as soon as possible.",
+    },
   };
   return (
     <section id="contact" className="contact-container">
@@ -16,11 +26,8 @@ const Contact = () => {
       </div>
       <div className="contact-section">
         <div className="contact-left">
-          <h1>Let's Talk</h1>
-          <p>
-            Feel free to reach out if you have any questions, suggestions, or
-            collaboration ideas. I’ll get back to you as soon as possible.
-          </p>
+          <h1>{texts[language].titleLeft}</h1>
+          <p>{texts[language].description}</p>
         </div>
         <form className="contact-right">
           <label htmlFor="">Your Name</label>
@@ -28,8 +35,14 @@ const Contact = () => {
           <label htmlFor="">Your Email</label>
           <input type="email" placeholder="Enter your email" name="email" />
           <label htmlFor="">Write your message here</label>
-          <textarea name="message" rows="8" placeholder="Enter your message"></textarea>
-          <button type="submit" className="contact-submit">Submit Now</button>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message"
+          ></textarea>
+          <button type="submit" className="contact-submit">
+            Submit Now
+          </button>
         </form>
       </div>
     </section>
