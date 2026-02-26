5e5c102cf74e28ba29e173a91be11ebf6b0fbab0
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-02-26 11:22:21 +0100

    refactor: add responsive styles to Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
       w="100%"
       justify="space-between"
       align="center"
-      p="4"
+      p={{ base: "3", md: "4" }}
       bg="gray.500"
     >
-      <Heading as="h1" size="2xl">
+      <Heading as="h1" size={{ base: "lg", md: "xl", lg: "2xl" }}>
         <Link href="#home">Theodor.</Link>
       </Heading>
-      <Text as="p" fontSize="2xl">
+      <Text as="p" fontSize={{ base: "lg", md: "xl", lg: "2xl" }}>
         &copy; 2026 Teodor Stevanovic. All rights reserved.
       </Text>
     </Flex>
