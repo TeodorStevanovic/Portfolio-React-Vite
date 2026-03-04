a79e1a828f32989a1da8c6e0015831b965427ae3
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-03-04 10:04:07 +0100

    feat: add theme support to Projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../../context/ThemeContext";
 import { Flex, Heading, SimpleGrid } from "@chakra-ui/react";
 import { projects } from "../../data/projectsData";
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <Flex id="projects" h="100vh" direction="column" align="center" gap="10" pt="90px">
-      <Heading as="h1" size="5xl">My Projects</Heading>
+    <Flex
+      id="projects"
+      h="100vh"
+      direction="column"
+      align="center"
+      gap="10"
+      pt="90px"
+      bg={theme === "light" ? "yellow.100" : "gray.900"}
+    >
+      <Heading as="h1" size="5xl">
+        My Projects
+      </Heading>
       <SimpleGrid columns={3} gap="20">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
